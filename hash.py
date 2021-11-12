472229a632e932ac25b84b51a4cd06e0796358a0
NatalieCrockett <77812301+NatalieCrockett@users.noreply.github.com>
2021-11-11 21:19:25 -0800

    Update hash.py

--- hash.py
+++ hash.py
@@ -10,18 +10,27 @@ class HashTable:
 
 	# set up the hash table, with no values in it
 	def __init__(self):
-		pass
+		self.most = 4
+		self.size = 0
+		self.table = [None] * self.most
 		
 	# given some input, figure out its hash values
 	# * You may use python hash() in this method
 	def __getKey(self, x):
-		pass
+		return hash(x) / self.most
 		
 	# rehash the table if it is not of appropriate size
 	# * Rehashing is required for this assignment, though this is private
-	# * I reserve the write to change the parameters of random list generation in autograder to force rehashing
+	# * I reserve the right to change the parameters of random list generation in autograder to force rehashing
 	def __rehash(self, m):
-		pass
+		if m <= 4:
+			return self
+		self.most = m
+		prev = self.table
+		for i in prev:
+			if i != None:
+				self.insert(i)
+		return
 		
 	# insert x into the hash table
 	# * Use some data structure to deal with collisions, such as:
@@ -33,12 +42,38 @@ class HashTable:
 	#   - Stack
 	#   - Queue
 	def insert(self, x):
-		pass
+		self.size = self.size + 1
+		if self.size > self.most//2:
+			self.__rehash(self.most * 2)
+		p = self.__getKey(x)
+		while self.table[p] != None:
+			p = (p + 1) / self.most
+		self.table[p] = x
+		return
 		
 	# remove x from the hash table	
 	def remove(self, x):
-		pass
+		self.size = self.size - 1
+		p = self.__getKey(x)
+		while self.table[p] != None:
+			p = (p + 1) / self.most
+		new = (p + 1) / self.most
+		while self.table[new] != None:
+			if self.__getKey(self.table[new]) > p:
+				new = (new + 1) / self.most
+			else:
+				p = new
+				new = (p + 1) / self.most
+		self.table[p] = None
+		if self.size < self.most // 4:
+			self.__rehash(self.most //2)
+		return
+		
 				
 	# check if x is in the hash table
 	def contains(self, x):
-		pass
+		p = self.__getKey(x)
+		while self.table[p] != None:
+			if self.table[p] == x:
+				return True
+		return False
